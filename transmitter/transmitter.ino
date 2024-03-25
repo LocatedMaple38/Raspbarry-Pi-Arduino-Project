ce958fad78f42c55f034920524e52c8b7e2f91fe
LocatedMaple38 <144717449+LocatedMaple38@users.noreply.github.com>
2024-03-25 19:51:26 +0000

    Update transmitter.ino

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,4 +1,5 @@
 #include <LiquidCrystal.h>
+#include <Wire.h>
 
 // pinassinmnt
 #define TX_Data 1
@@ -18,6 +19,9 @@ LiquidCrystal lcd_1(lcd_RS, lcd_E, lcd_D4, lcd_D5, lcd_D6, lcd_D7);
 const char *message = "Hello, Wornd!";
 
 void setup(){
+
+  Wire.begin();
+  
   //inisolize the lcd to a 16 char by 2 ln disply
   lcd_1.begin(20, 4);
 
@@ -27,7 +31,7 @@ void setup(){
   // set the curosor to top left and print the "message"
   lcd_1.setCursor(0, 0);
   lcd_1.print(message);
-
+/*
   // gose thrue all the byts in the "message"
   for(int byte_idx = 0; byte_idx < strlen(message); byte_idx++){
     char tx_byte = message[byte_idx];
@@ -59,9 +63,16 @@ void setup(){
     } 
 
   }
-
+*/
 }
 
 
 
-void loop(){}
+void loop(){
+  Wire.requestFrom(0, 6);    // request 6 bytes from slave device #8
+
+  while (Wire.available()) { // slave may send less than requested
+    char c = Wire.read(); // receive a byte as character
+    lcd_1.print(c, 0);         // print the character
+  }
+}
