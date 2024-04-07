7b4ea9e3ed5cc1071c141808f9f8224d8ef86380
LocatedMaple38 <144717449+LocatedMaple38@users.noreply.github.com>
2024-04-07 20:27:57 +0100

    Update receiver.ino

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,3 +1,5 @@
+#include <SoftwareSerial.h>
+
 #include <LiquidCrystal.h>
 
 // pinassinmnt
@@ -12,57 +14,64 @@
 
 LiquidCrystal lcd_1(lcd_RS, lcd_E, lcd_D4, lcd_D5, lcd_D6, lcd_D7);
 
-char message[100000000000];
+char message[1000];
 
 volatile byte rx_byte = 0;
 volatile int bit_position = 0;
 volatile bool update_LCD_1 = true;
 
-void setup(){
+void setup() {
 
+  Serial.begin(9600);
   //inisolize the lcd to a 20 char by 4 ln disply (defalt is 16 char by 2 ln display)
-  lcd_1.begin(20, 4);
-  
+  //lcd_1.begin(20, 4);
+
   pinMode(RX_Data, INPUT);
   pinMode(RX_Clock, INPUT);
   strcpy(message, "");
   //sets pin 'RX_Clock' to and intrupt that calls the "onClockPulse" whene the clock "rises" (form 0 -> 1)
   attachInterrupt(digitalPinToInterrupt(RX_Clock), onClockPulse, RISING);
+//  attachInterrupt(digitalPinToInterrupt(RX_Clock), onClockPulse, FALLING);
 }
 
-void onClockPulse(){
+void onClockPulse() {
   bool rx_bit = digitalRead(RX_Data);
 
-  if(bit_position == 8){
+  if (bit_position == 8) {
     bit_position = 0;
   }
-  
-  if(rx_bit){
+
+  if (rx_bit) {
     rx_byte  |= (0x80 >> bit_position);
   }
-  
+
   bit_position += 1;
-  
-  if(bit_position == 8){
+
+  if (bit_position == 8) {
     strncat(message, &rx_byte, 1);
   }
   // sets the boolean to true in tirn updatig the lcd
   update_LCD_1 = true;
 }
 
-void loop(){
-  if(update_LCD_1){
+void loop() {
+  if (update_LCD_1) {
     update_LCD_1 = false;
     // update the lcd
     lcd_1.setCursor(0, 0);
     lcd_1.print(message);
+    Serial.println(message);
     lcd_1.setCursor(0, 1);
-    
-    for(int i = 0; i < 8; i += 1){
-      if (i < bit_position){
+
+    for (int i = 0; i < 8; i += 1) {
+      if (i < bit_position) {
         lcd_1.print(rx_byte & (0x80 >> i) ? "1" : "0");
-      }else{
+
+        Serial.print(rx_byte & (0x80 >> i) ? "1" : "0");
+      } else {
+     
         lcd_1.print(" ");
+        
       }
     }
   }
